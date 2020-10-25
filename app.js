d8f277cfcacafa26871e5ed596ddbcf5c3cf8ef6
ortkyu <rtkstandart@yandex.ru>
2020-10-25 22:12:39 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+
+app = express()
+
+
+app.use(express.json({ extended: true })) // for parsing application/json
+
+app.use('/', require('./routes/articles.routes'))
+app.use('/more', require('./routes/articles.routes'))
+
+
+ const PORT = config.get('port') || 5000
+
+ async  function  start () {
+    try {
+        await mongoose.connect('mongodb+srv://john:111111jo@cluster0.wdfrb.azure.mongodb.net/news?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+
+        app.listen(PORT, () => console.log(`сервер старт ${PORT}`))
+    } catch (e) {
+        console.log('сервер не работает', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
+
+
